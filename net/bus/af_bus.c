4e262817d53359f27f1e56e7de38b2428ead1d88
Javier Martinez Canillas <javier.martinez@collabora.co.uk>
2012-05-23 12:41:02 +0200

    net: bus: don't check for socket type on bus_inq_len()
    SIOCINQ ioctl() command returns the amount of queued unread data in
    the receive buffer.
    
    It checked the socket type and returned the len of the peek packet in
    case of SOCK_DGRAM or it walked over the  skb receive_queue list and
    returned the sum of each packet len in case of SOCK_[STREAM | SEQPACKET].
    
    AF_BUS only supports SOCK_SEQPACKET sock type so this check is not needed.
    
    Signed-off-by: Javier Martinez Canillas <javier.martinez@collabora.co.uk>

--- net/bus/af_bus.c
+++ net/bus/af_bus.c
@@ -2055,15 +2055,8 @@ long bus_inq_len(struct sock *sk)
 		return -EINVAL;
 
 	spin_lock(&sk->sk_receive_queue.lock);
-	if (sk->sk_type == SOCK_STREAM ||
-	    sk->sk_type == SOCK_SEQPACKET) {
-		skb_queue_walk(&sk->sk_receive_queue, skb)
-			amount += skb->len;
-	} else {
-		skb = skb_peek(&sk->sk_receive_queue);
-		if (skb)
-			amount = skb->len;
-	}
+	skb_queue_walk(&sk->sk_receive_queue, skb)
+		amount += skb->len;
 	spin_unlock(&sk->sk_receive_queue.lock);
 
 	return amount;
