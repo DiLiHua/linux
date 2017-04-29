a57bd04dc09451b8ceebcfc49abe83c18e2aad09
Daniel Drake <drake@endlessm.com>
2017-04-29 09:42:37 -0600

    board: cleanups

--- sound/soc/intel/boards/bytcht_es8316.c
+++ sound/soc/intel/boards/bytcht_es8316.c
@@ -22,25 +22,19 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/platform_device.h>
-#include <linux/acpi.h>
 #include <linux/device.h>
-#include <linux/dmi.h>
 #include <linux/slab.h>
-#include <asm/cpu_device_id.h>
 #include <asm/platform_sst_audio.h>
 #include <linux/clk.h>
 #include <sound/pcm.h>
 #include <sound/pcm_params.h>
 #include <sound/soc.h>
-#include <sound/jack.h>
-#include "../../codecs/es8316.h"
 #include "../atom/sst-atom-controls.h"
 #include "../common/sst-acpi.h"
 #include "../common/sst-dsp.h"
+
 struct byt_cht_es8316_private {
 	struct clk *mclk;
-	struct snd_soc_jack jack;
-	struct cht_acpi_card *acpi_card;
 };
 
 #define CODEC_DAI1	"ES8316 HiFi"
@@ -58,19 +52,19 @@ static inline struct snd_soc_dai *get_codec_dai(struct snd_soc_card *card)
 }
 
 static int platform_clock_control(struct snd_soc_dapm_widget *w,
-				  struct snd_kcontrol *k, int  event)
+				  struct snd_kcontrol *k, int event)
 {
 	struct snd_soc_dapm_context *dapm = w->dapm;
 	struct snd_soc_card *card = dapm->card;
-	struct snd_soc_dai *codec_dai;
+	struct snd_soc_dai *codec_dai = get_codec_dai(card);
 	struct byt_cht_es8316_private *priv = snd_soc_card_get_drvdata(card);
 
-	codec_dai = get_codec_dai(card);
 	if (!codec_dai) {
 		dev_err(card->dev,
 			"Codec dai not found; Unable to set platform clock\n");
 		return -EIO;
 	}
+
 	if (SND_SOC_DAPM_EVENT_ON(event)) {
 		int ret = clk_prepare_enable(priv->mclk);
 		if (ret < 0) {
@@ -94,26 +88,23 @@ static const struct snd_soc_dapm_widget byt_cht_es8316_widgets[] = {
 			    SND_SOC_DAPM_POST_PMD),
 };
 
-
-
 static const struct snd_soc_dapm_route byt_cht_es8316_audio_map[] = {
-        {"MIC1", NULL, "Microphone 1"},
-        {"MIC2", NULL, "Microphone 2"},
- 
-        {"Headphone", NULL, "HPOL"},
-        {"Headphone", NULL, "HPOR"},
+	{"MIC1", NULL, "Microphone 1"},
+	{"MIC2", NULL, "Microphone 2"},
+
+	{"Headphone", NULL, "HPOL"},
+	{"Headphone", NULL, "HPOR"},
 
 	{"Playback", NULL, "ssp2 Tx"},
-        {"ssp2 Tx", NULL, "codec_out0"},
-        {"ssp2 Tx", NULL, "codec_out1"},
-        {"codec_in0", NULL, "ssp2 Rx" },
-        {"codec_in1", NULL, "ssp2 Rx" },
-        {"ssp2 Rx", NULL, "Capture"},
+	{"ssp2 Tx", NULL, "codec_out0"},
+	{"ssp2 Tx", NULL, "codec_out1"},
+	{"codec_in0", NULL, "ssp2 Rx" },
+	{"codec_in1", NULL, "ssp2 Rx" },
+	{"ssp2 Rx", NULL, "Capture"},
 
-        {"Headphone", NULL, "Platform Clock"},
-        {"Microphone 1", NULL, "Platform Clock"},
-        {"Microphone 2", NULL, "Platform Clock"},
-
+	{"Headphone", NULL, "Platform Clock"},
+	{"Microphone 1", NULL, "Platform Clock"},
+	{"Microphone 2", NULL, "Platform Clock"},
 };
 
 static const struct snd_kcontrol_new byt_cht_es8316_controls[] = {
@@ -122,15 +113,12 @@ static const struct snd_kcontrol_new byt_cht_es8316_controls[] = {
 	SOC_DAPM_PIN_SWITCH("Microphone 2"),
 };
 
-
-
 static int byt_cht_es8316_aif1_hw_params(struct snd_pcm_substream *substream,
-					struct snd_pcm_hw_params *params)
+					 struct snd_pcm_hw_params *params)
 
 {
 	struct snd_soc_pcm_runtime *rtd = substream->private_data;
 	struct snd_soc_dai *codec_dai = rtd->codec_dai;
-
 	int ret;
 
 	ret = snd_soc_dai_set_sysclk(codec_dai, 0, 19200000, SND_SOC_CLOCK_IN);
@@ -138,17 +126,16 @@ static int byt_cht_es8316_aif1_hw_params(struct snd_pcm_substream *substream,
 		dev_err(rtd->dev, "can't set codec clock %d\n", ret);
 		return ret;
 	}
-	
+
 	return 0;
 }
-
-
 
 static int byt_cht_es8316_init(struct snd_soc_pcm_runtime *runtime)
 {
 	struct snd_soc_card *card = runtime->card;
 	struct byt_cht_es8316_private *priv = snd_soc_card_get_drvdata(card);
-	printk("!!!!!!!!!!!!!!!!!!!!!!!Enter Into %s \n", __func__);
+	int ret;
+
 	card->dapm.idle_bias_off = true;
 
 	/*
@@ -160,15 +147,13 @@ static int byt_cht_es8316_init(struct snd_soc_pcm_runtime *runtime)
 	 * the clock first.
 	 */
 	ret = clk_prepare_enable(priv->mclk);
-	if(!ret)
-	{
-		printk("clk_prepare_enable, ret = 0\n");
+	if (!ret)
 		clk_disable_unprepare(priv->mclk);
-	}
+
 	ret = clk_set_rate(priv->mclk, 19200000);
 	if (ret)
-		printk("%s, unable to set MCLK rate\n", __func__);
-	printk("Exit %s\n", __func__);
+		dev_err(card->dev, "unable to set MCLK rate\n");
+
 	return 0;
 }
 
@@ -188,13 +173,14 @@ static int byt_cht_es8316_codec_fixup(struct snd_soc_pcm_runtime *rtd,
 	struct snd_interval *channels = hw_param_interval(params,
 						SNDRV_PCM_HW_PARAM_CHANNELS);
 	int ret;
-	printk("Enter into %s\n", __func__);
+
 	/* The DSP will covert the FE rate to 48k, stereo */
 	rate->min = rate->max = 48000;
 	channels->min = channels->max = 2;
 
 	/* set SSP2 to 24-bit */
 	params_set_format(params, SNDRV_PCM_FORMAT_S24_LE);
+
 	/*
 	 * Default mode for SSP configuration is TDM 4 slot, override config
 	 * with explicit setting to I2S 2ch 24-bit. The word length is set with
@@ -206,16 +192,16 @@ static int byt_cht_es8316_codec_fixup(struct snd_soc_pcm_runtime *rtd,
 				SND_SOC_DAIFMT_CBS_CFS
 		);
 	if (ret < 0) {
-		printk("can't set format to I2S, err %d\n", ret);
+		dev_err(rtd->dev, "can't set format to I2S, err %d\n", ret);
 		return ret;
 	}
 
 	ret = snd_soc_dai_set_tdm_slot(rtd->cpu_dai, 0x3, 0x3, 2, 24);
 	if (ret < 0) {
-		printk("can't set I2S config, err %d\n", ret);
+		dev_err(rtd->dev, "can't set I2S config, err %d\n", ret);
 		return ret;
 	}
-	printk("Exit %s\n", __func__);
+
 	return 0;
 }
 
@@ -275,10 +261,10 @@ static struct snd_soc_dai_link byt_cht_es8316_dais[] = {
 	{
 		.name = "SSP2-Codec",
 		.id = 1,
-		.cpu_dai_name = "ssp2-port", /* overwritten for ssp0 routing */
+		.cpu_dai_name = "ssp2-port",
 		.platform_name = "sst-mfld-platform",
 		.no_pcm = 1,
-		.codec_dai_name = "ES8316 HiFi", /* changed w/ quirk */
+		.codec_dai_name = "ES8316 HiFi",
 		.codec_name = "i2c-ESSX8316:00",
 		.dai_fmt = SND_SOC_DAIFMT_I2S | SND_SOC_DAIFMT_NB_NF
 						| SND_SOC_DAIFMT_CBS_CFS,
@@ -308,22 +294,16 @@ static struct snd_soc_card byt_cht_es8316_card = {
 
 static int snd_byt_cht_es8316_mc_probe(struct platform_device *pdev)
 {
-	int ret_val = 0;
+	int ret = 0;
 	struct byt_cht_es8316_private *priv;
-	printk("!!!!!!!!!!!!!!!!!!!!!!!Enter Into %s \n", __func__);
+
 	priv = devm_kzalloc(&pdev->dev, sizeof(*priv), GFP_ATOMIC);
 	if (!priv)
 		return -ENOMEM;
+
 	/* register the soc card */
 	byt_cht_es8316_card.dev = &pdev->dev;
-
 	snd_soc_card_set_drvdata(&byt_cht_es8316_card, priv);
-	ret_val = snd_soc_register_card(&byt_cht_es8316_card);
-	if (ret_val) {
-		printk("snd_soc_register_card failed %d\n", ret_val);
-		return ret_val;
-	}
-	platform_set_drvdata(pdev, &byt_cht_es8316_card);
 
 	priv->mclk = devm_clk_get(&pdev->dev, "pmc_plt_clk_3");
 	if (IS_ERR(priv->mclk)) {
@@ -334,8 +314,13 @@ static int snd_byt_cht_es8316_mc_probe(struct platform_device *pdev)
 		return ret;
 	}
 
-	printk("Exit %s\n", __func__);
-	return ret_val;
+	ret = devm_snd_soc_register_card(&pdev->dev, &byt_cht_es8316_card);
+	if (ret) {
+		dev_err(&pdev->dev, "snd_soc_register_card failed %d\n", ret);
+		return ret;
+	}
+	platform_set_drvdata(pdev, &byt_cht_es8316_card);
+	return ret;
 }
 
 static struct platform_driver snd_byt_cht_es8316_mc_driver = {
@@ -346,7 +331,7 @@ static struct platform_driver snd_byt_cht_es8316_mc_driver = {
 };
 
 module_platform_driver(snd_byt_cht_es8316_mc_driver);
-MODULE_DESCRIPTION("ASoC Intel(R) Cherrytrail Machine driver");
+MODULE_DESCRIPTION("ASoC Intel(R) Baytrail/Cherrytrail Machine driver");
 MODULE_AUTHOR("DavidYang <yangxiaohua@everest-semi.com / info@everest-semi.com>");
 MODULE_LICENSE("GPL v2");
 MODULE_ALIAS("platform:bytcht_es8316");
